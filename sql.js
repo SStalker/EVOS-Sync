0d393eeeb46ca041ecb5f6a8d3a6bd62a350b2da
Christian Wansart <cwansart@users.noreply.github.com>
2016-04-10 10:23:50 +0200

    Created Database module

--- sql.js
+++ sql.js
@@ -1,32 +1,44 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'evos',
-	password: 'evos',
-	database: 'evos'
-});
 
-var quizId = 1;
-var userId = 1;
+var Database = function() {
+	this.connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'evos',
+		password: 'evos',
+		database: 'evos'
+	});
 
-var query = 'SELECT users.name\
-	FROM quizzes, categories, users\
-	WHERE quizzes.category_id = categories.id\
-	AND categories.user_id = users.id\
-	AND users.id = ' + userId + '\
-	AND quizzes.id = ' + quizId + '\
-	AND quizzes.isActive = 1';
+	this.connection.connect();
+}
 
-connection.connect();
-connection.query(query,
-	function(err, rows, fields) {
-		if(err == null) {
-			console.log(err);
-		} else {
-			if(rows != null) {
-				console.log(rows);
+Database.prototype.close = function() {
+	this.connection.end();
+};
+
+// Checks if the given user is the owner of the quiz.
+Database.prototype.isOwner = function(userId, quizId, callbackTrue, callbackFalse) {
+	var query = 'SELECT users.name\
+		FROM quizzes, categories, users\
+		WHERE quizzes.category_id = categories.id\
+		AND categories.user_id = users.id\
+		AND users.id = ' + parseInt(userId) + '\
+		AND quizzes.id = ' + parseInt(quizId);
+
+	this.connection.query(query,
+		function(err, rows, fields) {
+			if(err === null) {
+				if(rows.length == 1) {
+					if(callbackTrue !== undefined) {
+						callbackTrue();
+					}
+				} else {
+					if(callbackFalse !== undefined) {
+						callbackFalse();
+					}
+				}
 			}
 		}
-	}
-);
-connection.end();
+	);
+};
+
+module.exports = Database;
