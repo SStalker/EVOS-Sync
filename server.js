72504850f6a1630c0958218d0dfb977db06f7fba
Christian Wansart <cwansart@users.noreply.github.com>
2016-04-16 17:30:04 +0200

    Implemented logon message

--- server.js
+++ server.js
@@ -1,57 +1,109 @@
 var WebSocketServer = require('ws').Server;
 var Database = require('./sql.js');
 
-var wss = new WebSocketServer({port:8000});
+var wss = new WebSocketServer({path: '/sync', port: 8000});
 var db = new Database();
 var sessions = [];
 
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
+        message = JSON.parse(message);
         switch(message.type) {
             case 'start':
-                startQuiz(message);
+                startQuiz(ws, message);
                 break;
+            case 'logon':
+                logon(ws, message);
+            default:
+                console.log('Received an invalid message.');
         }
     });
 });
 
 // Starts a quiz
-function startQuiz(message) {
+function startQuiz(ws, message) {
     // Check if the message contains the required data
-    if(message.quiz_id === undefined || message.session_id === undefined || user_id === undefined) {
+    if(message.quiz_id === undefined || message.session_id === undefined || message.user_id === undefined) {
         console.log('Invalid start message:');
         console.log(message);
+        sendResponse(ws, {
+            type: 'start',
+            successful: false,
+            reason: 'message is invalid'
+        });
         return false;
     }
 
     // Check if the session is already "online"
-    if(sessions[message.quiz_id] !== undefined) {
-        console.log('Quiz is already started:');
+    if(sessions[message.quiz_id] === undefined) {
+        // Check if user is owner of quiz. If so, add the quiz to the
+        // sessions list.
+        db.isOwner(message.user_id, message.quiz_id, function() {
+            sessions[message.quiz_id] = {
+                session_id: message.session_id,
+                user_ws: ws,
+                attendees: []
+            };
+        }, function() {
+            console.log('User is not the owner of the quiz:');
+            console.log(message);
+            sendResponse(ws, {
+                type: 'start',
+                successful: false,
+                reason: 'You are not the owner of the quiz.'
+            });
+            return false;
+        });
+    }
+
+    sendResponse(ws, {
+        type: 'start',
+        successful: true
+    });
+}
+
+// Adds an attendee to the sessions list
+function logon(ws, message) {
+    if(message.quiz_id === undefined) {
+        console.log('Invalid logon message');
         console.log(message);
+        sendResponse(ws, {
+            type: 'logon',
+            successful: false,
+            reason: 'Missing quiz_id.'
+        });
         return false;
     }
 
-    // Check if user is owner of quiz. If so, add the quiz to the
-    // sessions list.
-    db.isOwner(message.user_id, message.quiz_id, function() {
-        sessions[message.quiz_id] = {
-            session_id: message.session_id,
-            attendees: []
-        };
+    // If not nickname was given, we assign a default one.
+    nickname = nickname || 'anon Alfred;
 
-        sendResponse({
-            type: 'accept'
+    if(session[message.quiz_id] === undefined) {
+        sendResponse(ws, {
+            type: 'logon',
+            successful: false,
+            reason: 'Quiz is not open.'
         });
-    }, function() {
-        console.log('User is not the owner of the quiz:');
-        console.log(message);
+        return false;
+    }
+
+    // Infom Attendee
+    sendResponse(ws, {
+        type: 'logon',
+        successful: true
+    });
+
+    // Inform the User of new logon
+    sendResponse(sessions[message.quiz_id].user_ws, {
+        type: 'logon',
+        nickname: nickname
     });
 }
 
 // Directly answer to the current client
-function sendResponse(message) {
+function sendResponse(ws, message) {
     try {
-        ws.send(message);
+        ws.send(JSON.stringify(message));
     } catch(e) {
         console.log('Error while sending response:');
         console.log(e);
